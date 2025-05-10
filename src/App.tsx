7071621bd9b35c656f2e6a669bd212dbb974b3db
hinajawaid <hinajawaid.h@gmail.com>
2025-05-10 22:05:08 +0200

    1. Added input field for searching and filtered characters based on input. 2. Displayed error message when no matching characters are found.

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,28 @@ import CharacterCard from "./CharacterCard.tsx";
 
 function App() {
     const [characters] = useState(response.results)  // Storing character data. Using the mock data here
+    const[searchCharacter, setSearchCharacter] = useState("")//state to store input
+    const filteredCharacters =
+        characters.filter((character) =>
+            character.name.toLowerCase().includes(searchCharacter.toLowerCase())
+    ); //character filterization applied and stored back
+
     return (
         <>
-            {characters.map((character)=>
+            {/* Input field to search characters */}
+            <input onChange={(e)=>setSearchCharacter(e.target.value)}/> {/* Update searchCharacter*/}
+            {filteredCharacters.map((character)=>
                 <CharacterCard
                     key={character.id} //key should be on the root element, Key prop for efficient rendering
                     name ={character.name}
                     image = {character.image}
                 />
             )}
+            {/* Error message on no match */}
+            {searchCharacter && filteredCharacters.length === 0  //searchCharacter → input is not empty and filteredCharacters.length === 0 → no match found
+            && <p>Character not Found!</p>}
         </>
+        //updated from characters.map to filteredCharacters.map  to display only the filtered characters based on the search input.
     );
 }
 
