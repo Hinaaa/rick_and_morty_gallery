30a8cd51370d99250fffb027a2aaee445d2dfb2a
hinajawaid <hinajawaid.h@gmail.com>
2025-05-11 15:04:55 +0200

    useNavigate hook used. 1. Changed Link Component chacrters to button tags in Header 2. Used buttons onClick event to call useNavigate hook.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,19 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Header() {
+    const nav = useNavigate() //useNavigate hook
+    function gotoCharacters() { //function.  hidden in function
+        nav("/characters")
+    }
+    //make another function if want for other navigation example:  function goHome()
     return(
        <>
+
            <h1>Rick and Morty App</h1>
        <Link to={"/"}>Greetings </Link>
-        <Link to={"characters"}>Character Gallery </Link>
+           {/*Changed to button: <Link to={"characters"}>Character Gallery </Link>*/}
+
+           <button onClick={gotoCharacters}>Characters</button>
        </>
     )
 }
